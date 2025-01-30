9072a0657d46b6343079b5939ddbe85dab9acce7
Xamacardoso <xcardoso1997@gmail.com>
2025-01-30 00:17:44 -0300

    Add (Rede Social): Adicionados os métodos ejeição de solicitação e adicionar interação. Refatoração de métodos que buscavam por string

--- redeSocial.ts
+++ redeSocial.ts
@@ -1,6 +1,8 @@
+import Interacao from "./entities/interacao";
 import Perfil from "./entities/perfil";
 import PerfilAvancado from "./entities/perfilAvancado";
 import Publicacao from "./entities/publicacao";
+import PublicacaoAvancada from "./entities/publicacaoAvancada";
 
 /*2) Implementação da Classe RedeSocial (2,0 pontos) 
  
@@ -20,11 +22,11 @@ o Gerenciamento de Publicações:
     perfil. ?
 o Gerenciamento de solicitacoes: 
     ▪ Enviar solicitações de amizade; X
-    ▪ Aceitar solicitações; 
-    ▪ Recusar solicitações. 
+    ▪ Aceitar solicitações; X
+    ▪ Recusar solicitações.  X
 o Gerenciamento de Interações: 
     ▪ Adicionar interações a publicações avançadas, garantindo que um usuário não 
-    interaja mais de uma vez na mesma publicação. 
+    interaja mais de uma vez na mesma publicação.  X
     
     Nessa  classe,  devem  ser  passados  aos  métodos  preferencialmente  ids,  apelidos,  e-mails  ou 
     outros atributos únicos. Daí os objetos são consultados nos arrays e a partir daí serão manipulados*/
@@ -76,22 +78,12 @@ export default class RedeSocial {
     }
 
 
-    public adicionarPublicacao(conteudo: string, perfilId: string): void {
-        const perfil : Perfil | undefined = this.buscarPerfilPorId(perfilId);
-        if (!perfil) {
-            throw new Error("Perfil inválido.");
-        }
+    public adicionarPublicacao(conteudo: string, perfil: Perfil): void {
         const publicacao = new Publicacao(conteudo, perfil);
         this._publicacoes.push(publicacao);
     }
     
-    public enviarSolicitacao(emissorId: string, receptorId: string): void {
-        const emissor: Perfil | undefined = this.buscarPerfilPorId(emissorId);
-        const receptor: Perfil | undefined = this.buscarPerfilPorId(receptorId);
-        if (!emissor || !receptor) {
-            throw new Error("Emissor ou receptor inválidos!");
-        }
-
+    public enviarSolicitacao(emissor: Perfil, receptor: Perfil): void {
         if (!this._solicitacoes.has(receptor)) {
             this._solicitacoes.set(receptor, []);
         }
@@ -99,22 +91,57 @@ export default class RedeSocial {
         this._solicitacoes.get(receptor)!.push(emissor);
     }
 
-    public aceitarSolicitacao(emissorId: string, receptorId: string): void {
-        const emissor: Perfil | undefined = this.buscarPerfilPorId(emissorId);
-        const receptor: Perfil | undefined = this.buscarPerfilPorId(receptorId);
-        if (!emissor || !receptor) {
-            throw new Error("Emissor ou receptor inválidos!");
+    private existeSolicitacaoReceptor(receptor: Perfil): boolean {
+        return this._solicitacoes.has(receptor);
+    }
+
+    private existeSolicitacaoEmissor(solicitacoes: Perfil[], emissor: Perfil): boolean {
+        return solicitacoes.find(em => em.id === emissor.id) !== undefined;
+    }
+
+    public removerSolicitacao(emissor: Perfil, receptor: Perfil): void {
+        const solicitacoesAtlz = this._solicitacoes.get(receptor)!.filter(em => em.id !== emissor.id);
+        this._solicitacoes.set(receptor, solicitacoesAtlz);
+    }
+
+    public aceitarSolicitacao(emissor: Perfil, receptor: Perfil): void {
+        if (this.existeSolicitacaoReceptor(receptor) === false) {
+            throw new Error("Nenhuma solicitação encontrada para este receptor!");
         }
     
-        if (!this._solicitacoes.has(receptor)) {
-            throw new Error("Nenhuma solicitação encontrada!");
-        }
-    
-        if (!this._solicitacoes.get(receptor)?.find(em => em.id === emissorId)) {
+        if (!this.existeSolicitacaoEmissor(this._solicitacoes.get(receptor)!, emissor)) {
             throw new Error("Solicitação inválida!");
         }
         
         emissor.adicionarAmigo(receptor);
     }
-            
+
+    public rejeitarSolicitacao(emissor: Perfil, receptor: Perfil): void {
+        if (!this.existeSolicitacaoReceptor(receptor)) {
+            throw new Error("Nenhuma solicitação encontrada!");
+        }
+    
+        if (!this.existeSolicitacaoEmissor(this._solicitacoes.get(receptor)!, emissor)) {
+            throw new Error("Solicitação inválida!");
+        }
+
+        this.removerSolicitacao(emissor, receptor);
+    }
+
+    public existeInteracao(perfil: Perfil, publicacao: PublicacaoAvancada): boolean {
+        return publicacao.interacoes.find(interacao => interacao.id === perfil.id) !== undefined;
+    }
+    
+    public adicionarInteracao(publicacao: Publicacao, perfil: Perfil, interacao: Interacao): void {
+
+        if (!(publicacao instanceof PublicacaoAvancada)){
+            throw new Error("Publicação inválida para adicionar interações!");
+        }
+
+        if (this.existeInteracao(perfil, publicacao)) {
+            throw new Error("Interação já existente!");
+        }
+
+        publicacao.addInteracao(interacao);
+    }
 }
